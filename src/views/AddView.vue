a00edd71ffaa5ee981a77ef02f03bd6557716d3e
Manoz <manoz@outlook.com>
2023-11-12 12:35:51 +0100

    format

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -1,26 +1,36 @@
 <template>
-  <form class="max-w-2xl mx-auto flex items-center justify-center gap-4 flex-col" @submit.prevent="submitPhrase">
-    <input class="w-full p-4 focus:ring-primary focus:border-primary" type="text" v-model="newPhrase"
-      placeholder="Ajoute une nouvelle phrase" />
-    <button type="submit"
-      class="w-full rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-colors duration-300">Envoyer
-      OMG !</button>
+  <form
+    class="max-w-2xl mx-auto flex items-center justify-center gap-4 flex-col"
+    @submit.prevent="submitPhrase"
+  >
+    <input
+      class="w-full p-4 focus:ring-primary focus:border-primary"
+      type="text"
+      v-model="newPhrase"
+      placeholder="Ajoute une nouvelle phrase"
+    />
+    <button
+      type="submit"
+      class="w-full rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-colors duration-300"
+    >
+      Envoyer OMG !
+    </button>
   </form>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { usePhrasesStore } from '@/stores/phrases';
+import { ref } from 'vue'
+import { usePhrasesStore } from '@/stores/phrases'
 
-const newPhrase = ref('');
-const phrasesStore = usePhrasesStore();
+const newPhrase = ref('')
+const phrasesStore = usePhrasesStore()
 
 const submitPhrase = () => {
   if (newPhrase.value === '') {
-    return;
+    return
   }
 
-  phrasesStore.addPhrase(newPhrase.value);
-  newPhrase.value = '';
-};
+  phrasesStore.addPhrase(newPhrase.value)
+  newPhrase.value = ''
+}
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
 <template>
   <main>
     <div class="max-w-3xl mx-auto">
-      <p class="text-center text-2xl leading-8 sm:text-4xl sm:leading-[3rem] font-light min-h-[170px]">
+      <p
+        class="text-center text-2xl leading-8 sm:text-4xl sm:leading-[3rem] font-light min-h-[170px]"
+      >
         {{ phrasesStore.randomPhrase }}
       </p>
 
       <div class="mx-auto flex items-center justify-center my-8">
-        <button type="button" @click="nextPhrase" aria-label="Afficher une autre phrase"
-          class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-colors duration-300">
+        <button
+          type="button"
+          @click="nextPhrase"
+          aria-label="Afficher une autre phrase"
+          class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-colors duration-300"
+        >
           Une autre !
         </button>
       </div>
@@ -16,15 +22,15 @@
 </template>
 
 <script setup>
-import { usePhrasesStore } from '@/stores/phrases';
+import { usePhrasesStore } from '@/stores/phrases'
 
-const phrasesStore = usePhrasesStore();
+const phrasesStore = usePhrasesStore()
 
 phrasesStore.loadPhrases().then(() => {
-  phrasesStore.refreshRandomPhrase();
-});
+  phrasesStore.refreshRandomPhrase()
+})
 
 const nextPhrase = () => {
-  phrasesStore.refreshRandomPhrase();
-};
+  phrasesStore.refreshRandomPhrase()
+}
 </script>
