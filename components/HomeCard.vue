73284f0ff966ce5769c08c40bb02c98cc33d5727
Manoz <manoz@outlook.com>
2025-11-28 14:33:36 +0100

    Replace RouterLink with NuxtLink in HomeCard component

--- components/HomeCard.vue
+++ components/HomeCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="transition-transform duration-300 col-span-3">
-    <RouterLink
+    <NuxtLink
       class="flex flex-col items-center h-full rounded-2xl px-4 md:px-6 py-6 sm:py-8 border border-gray-400 bg-white focus-within:ring-2 focus-within:ring-offset-2 hover:border-gray-500 hover:shadow-md outline-none shadow-sm transition duration-200"
       :to="link"
       :class="{
@@ -14,13 +14,11 @@
       <p class="mt-4 text-sm sm:text-base">
         {{ desc }}
       </p>
-    </RouterLink>
+    </NuxtLink>
   </div>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router'
-
 defineProps({
   title: {
     type: String,
